d3347763e3efe8006e278e678eb861d94bfcac8e
Mingshan Lei <leimingshan116@gmail.com>
2015-05-12 22:07:37 +0800

    add UserRepositoryTest junit test

---
+++ src/test/java/com/bubbles/server/domain/UserRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.bubbles.server.domain;
+
+import com.bubbles.server.BubblesApplication;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by LMSH on 2015/5/12.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BubblesApplication.class)
+@WebAppConfiguration
+public class UserRepositoryTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void getUser() throws Exception {
+        this.mockMvc.perform(get("/people/1").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(jsonPath("$.gender").value("m"));
+    }
+}
