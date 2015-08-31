1efc3722ed16dc31ce18c1aaa37280c8f543d408
Mingshan Lei <leimingshan116@gmail.com>
2015-08-31 15:41:03 +0800

    add: download url

--- src/main/java/com/bubbles/server/web/MainController.java
+++ src/main/java/com/bubbles/server/web/MainController.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- *
- *
  * @author Mingshan Lei
  * @since 2015/8/31.
  */
@@ -17,12 +15,23 @@ public class MainController {
     public Version getMinVersion() {
         Version version = new Version();
         version.setForceUpdateMinVersion(1);
+        version.setUrl("http://download.test.com/temp.apk");
         return version;
     }
 
     public class Version {
 
+        public String url;
+
         public Integer forceUpdateMinVersion;
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
 
         public Integer getForceUpdateMinVersion() {
             return forceUpdateMinVersion;
