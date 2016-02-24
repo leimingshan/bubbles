3c8ed52ce5e5a0faf62ab5a13b0594d550a461aa
Mingshan Lei <leimingshan116@gmail.com>
2016-02-24 09:33:59 +0800

    update: spring version & fix test

--- src/test/java/com/bubbles/server/web/UserControllerTest.java
+++ src/test/java/com/bubbles/server/web/UserControllerTest.java
@@ -9,10 +9,12 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.Assert.assertEquals;
@@ -26,6 +28,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = BubblesApplication.class)
 @WebAppConfiguration
+@Transactional
+@Sql({"/test-schema.sql", "/test-user-data.sql"})
 public class UserControllerTest {
 
     @Autowired
@@ -40,7 +44,7 @@ public class UserControllerTest {
 
     @Test
     public void testUserByDeviceId() throws Exception {
-        String jsonString = this.mockMvc.perform(get("/users/search/deviceId/000000000000000")
+        String jsonString = this.mockMvc.perform(get("/users/search/deviceId/000000000010000")
                 .characterEncoding("UTF-8")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andReturn().getResponse().getContentAsString();
@@ -50,6 +54,6 @@ public class UserControllerTest {
         ObjectMapper mapper = new ObjectMapper();
         User user = mapper.readValue(jsonString, User.class);
         assertNotNull(user);
-        assertEquals(user.getDeviceId(), "000000000000000");
+        assertEquals(user.getDeviceId(), "000000000010000");
     }
 }
