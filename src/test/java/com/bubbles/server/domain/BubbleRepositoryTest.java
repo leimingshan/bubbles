9ec3017e881e2f3d533eac304306761a4643206e
Mingshan Lei <leimingshan116@gmail.com>
2016-02-25 11:17:51 +0800

    add: more test samples

--- src/test/java/com/bubbles/server/domain/BubbleRepositoryTest.java
+++ src/test/java/com/bubbles/server/domain/BubbleRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -31,9 +32,64 @@ public class BubbleRepositoryTest {
     private BubbleRepository bubbleRepository;
 
     @Test
-    public void testFindByUserId() {
+    public void testFindByUserId() throws Exception {
         List<Bubble> bubbleList = bubbleRepository.findByUserId(10000);
         assertNotNull(bubbleList);
         assertEquals(2, bubbleList.size());
+    }
+
+    @Test
+    public void testFindByUserIdOrderByTime() throws Exception {
+        List<Bubble> bubbleList = bubbleRepository.findByUserIdOrderByTime(10000);
+        assertNotNull(bubbleList);
+        assertEquals(2, bubbleList.size());
+    }
+
+    @Test
+    public void testFindParticipateBubblesByUserId() throws Exception {
+        List<Bubble> bubbleList = bubbleRepository.findByUserIdOrderByTime(10000);
+        assertNotNull(bubbleList);
+        assertEquals(2, bubbleList.size());
+    }
+
+    @Test
+    public void testFindRepliesByBubbleIdOrderByTime() throws Exception {
+        List<Bubble> bubbleList = bubbleRepository.findRepliesByBubbleIdOrderByTime(10000L);
+        assertNotNull(bubbleList);
+        assertEquals(1, bubbleList.size());
+    }
+
+    @Test
+    public void testFindTop5ByParentBubbleId() throws Exception {
+        List<Bubble> bubbleList = bubbleRepository.findTop5ByParentBubbleId(10000L, new PageRequest(0, 10));
+        assertNotNull(bubbleList);
+        assertEquals(1, bubbleList.size());
+    }
+
+    @Test
+    public void testFindHotBubbles() throws Exception {
+        List<Bubble> bubbleList = bubbleRepository.findHotBubbles(50.0, 52.0, 40.0, 41.0, new PageRequest(0, 10));
+        assertNotNull(bubbleList);
+        assertEquals(1, bubbleList.size());
+    }
+
+    @Test
+    public void testFindNewBubbles() throws Exception {
+        List<Bubble> bubbleList = bubbleRepository.findNewBubbles(50.0, 52.0, 40.0, 41.0, new PageRequest(0, 10));
+        assertNotNull(bubbleList);
+        assertEquals(1, bubbleList.size());
+    }
+
+    @Test
+    public void testFindBriefBubbles() throws Exception {
+        List<Bubble> bubbleList = bubbleRepository.findBriefBubbles();
+        assertNotNull(bubbleList);
+        assertEquals(2, bubbleList.size());
+    }
+
+    @Test
+    public void testAddScoreById() throws Exception {
+        int result = bubbleRepository.addScoreById(10000, 10);
+        assertEquals(result, 1);
     }
 }
