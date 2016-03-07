d82f27a8194fdcf0d4a0bffaa004ce68c02f6ee3
Mingshan Lei <leimingshan116@gmail.com>
2016-03-07 16:32:34 +0800

    add: test for BubbleController

---
+++ src/test/java/com/bubbles/server/web/BubbleControllerTest.java
@@ -0,0 +1,154 @@
+package com.bubbles.server.web;
+
+import com.bubbles.server.BubblesApplication;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Test for User web controller.
+ *
+ * @author Mingshan Lei
+ * @since 2016/3/7
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BubblesApplication.class)
+@WebAppConfiguration
+@Transactional
+@Sql({"/test-schema.sql", "/test-user-data.sql"})
+public class BubbleControllerTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void testGetBubbleById() throws Exception {
+        this.mockMvc.perform(get("/bubbles/10000")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andExpect(jsonPath("$.id").value(10000));
+    }
+
+    @Test
+    public void testGetRepliesByBubbleId() throws Exception {
+        this.mockMvc.perform(get("/bubbles/10000/replies")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andExpect(jsonPath("$", hasSize(1)));
+    }
+
+    @Test
+    public void testGetTop5RepliesByBubbleId() throws Exception {
+        this.mockMvc.perform(get("/bubbles/10000/replies/top")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andExpect(jsonPath("$", hasSize(1)));
+    }
+
+    @Test
+    public void testGetBriefBubbles() throws Exception {
+        this.mockMvc.perform(get("/bubbles/brief")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andExpect(jsonPath("$", hasSize(2)));
+    }
+
+    @Test
+    public void testGetBubblesByLocation() throws Exception {
+        this.mockMvc.perform(get("/bubbles")
+            .param("longitude", "116.36480").param("latitude", "39.969400").param("type", "hot")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andExpect(jsonPath("$", hasSize(2)));
+
+        this.mockMvc.perform(get("/bubbles")
+            .param("longitude", "116.36480").param("latitude", "39.969400").param("type", "new")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andExpect(jsonPath("$", hasSize(2)));
+
+        this.mockMvc.perform(get("/bubbles")
+            .param("longitude", "190").param("latitude", "39.969400").param("type", "new")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isUnprocessableEntity())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
+        this.mockMvc.perform(get("/bubbles")
+            .param("longitude", "-200").param("latitude", "39.969400").param("type", "new")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isUnprocessableEntity())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
+        this.mockMvc.perform(get("/bubbles")
+            .param("longitude", "116.36480").param("latitude", "101").param("type", "new")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isUnprocessableEntity())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
+        this.mockMvc.perform(get("/bubbles")
+            .param("longitude", "116.36480").param("latitude", "-100").param("type", "new")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isUnprocessableEntity())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
+        this.mockMvc.perform(get("/bubbles")
+            .param("longitude", "116.36480").param("latitude", "39.969400").param("type", "undefined")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isUnprocessableEntity())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
+        this.mockMvc.perform(get("/bubbles")
+            .param("longitude", "120.36480").param("latitude", "39.969400").param("type", "new")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andExpect(jsonPath("$", hasSize(0)));
+    }
+
+    @Test
+    public void testAddBubbleScoreById() throws Exception {
+        this.mockMvc.perform(post("/bubbles/10000/score")
+            .param("add", "1")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andExpect(content().string("1"));
+
+        this.mockMvc.perform(post("/bubbles/100/score")
+            .param("add", "1")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNotFound())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+}
--- src/test/resources/test-user-data.sql
+++ src/test/resources/test-user-data.sql
@@ -9,8 +9,8 @@ SET FOREIGN_KEY_CHECKS=1;
 INSERT INTO `user` VALUES ('10000', '000000000010000', '12345678912345678912', '10887322133', 'rayman', 'f', 'http://112.124.56.38/avatar/10000.jpg', '56', NOW(), NOW());
 INSERT INTO `user` VALUES ('10001', '000000000010001', '00000000000000012345','18734920714', 'liu', 'm', 'http://112.124.56.38/avatar/10001.jpg', '60', NOW(), NOW());
 
-INSERT INTO `bubble` VALUES ('10000', null, null, '10000', 'hellohello', '0', '1', '67', '2015-05-21 16:57:40', '2015-05-21 16:57:42', '50.11', '40.33', '1000');
-INSERT INTO `bubble` VALUES ('10001', '10000', '10000', '10000', 'hello world', '0', '0', '0', '2015-05-24 15:16:45', '2015-05-24 15:16:48', '28.9', '38.9999999998', '1000');
+INSERT INTO `bubble` VALUES ('10000', null, null, '10000', 'hellohello', '0', '1', '67', '2015-05-21 16:57:40', '2015-05-21 16:57:42', '39.963637', '116.36496', '2000');
+INSERT INTO `bubble` VALUES ('10001', '10000', '10000', '10000', 'hello world', '0', '0', '60', '2015-05-24 15:16:45', '2015-05-24 15:16:48', '39.969435', '116.364061', '2000');
 
 INSERT INTO `report` (`id`, `bubble_id`, `category`, `content`, `created_date`, `modified_date`) VALUES ('1', '10000', '1', '内容不符合', NOW(), NOW());
 INSERT INTO `report` (`id`, `bubble_id`, `category`, `content`, `created_date`, `modified_date`) VALUES ('2', '10000', '2', '内容质量低', NOW(), NOW());
